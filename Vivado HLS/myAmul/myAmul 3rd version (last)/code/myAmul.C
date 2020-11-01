9967cb0a703a0c189e48714c18b16a65d0746ac9
iordtila <73786002+iordtila@users.noreply.github.com>
2020-11-01 17:22:26 +0200

    Update myAmul.C

--- Vivado HLS/myAmul/myAmul 3rd version (last)/code/myAmul.C
+++ Vivado HLS/myAmul/myAmul 3rd version (last)/code/myAmul.C
@@ -16,7 +16,6 @@ void myAmul
 	const int* lPtr
 )
 {
-
 #pragma HLS INTERFACE m_axi depth=3756 port=uPtr offset=slave bundle=gmem3
 #pragma HLS INTERFACE m_axi depth=3756 port=lPtr offset=slave bundle=gmem3
 #pragma HLS INTERFACE m_axi depth=3756 port=upperPtr offset=slave bundle=gmem2
@@ -24,7 +23,6 @@ void myAmul
 #pragma HLS INTERFACE m_axi depth=525 port=psiPtr offset=slave bundle=gmem1
 #pragma HLS INTERFACE m_axi depth=525 port=diagPtr offset=slave bundle=gmem0
 #pragma HLS INTERFACE m_axi depth=525 port=ApsiPtr offset=slave bundle=gmem0
-
 #pragma HLS INTERFACE s_axilite port=nCells bundle=control
 #pragma HLS INTERFACE s_axilite port=nFaces bundle=control
 #pragma HLS INTERFACE s_axilite port=ApsiPtr bundle=control
@@ -37,14 +35,12 @@ void myAmul
 #pragma HLS INTERFACE s_axilite port=return bundle=control
 
 
-
 	double temp_ApsiPtr[525];
 
 	for (int cell=0; cell<nCells; cell++)
     	{
 		#pragma HLS LOOP_TRIPCOUNT min=0 max=525
 		#pragma HLS PIPELINE
-		
 		temp_ApsiPtr[cell]= diagPtr[cell]*psiPtr[cell];
     	}
 
@@ -52,8 +48,7 @@ void myAmul
     	{
 		#pragma HLS DEPENDENCE variable=temp_ApsiPtr intra false
     		#pragma HLS LOOP_TRIPCOUNT min=0 max=3756
-		#pragma HLS PIPELINE //Achieved II=5
-
+		#pragma HLS PIPELINE ΙΙ=5
     		temp_ApsiPtr[lPtr[face]] += upperPtr[face]*psiPtr[uPtr[face]];
     		temp_ApsiPtr[uPtr[face]] += lowerPtr[face]*psiPtr[lPtr[face]];
     	}
